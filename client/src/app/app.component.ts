04e101fcb39f30fdabea200696f383a9d3821fba
natia02 <natia2.pruidze@outlook.com>
2024-07-08 14:01:26 +0400

    end of section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import e from 'express';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5132/api/users').subscribe({
+      next: response => this.users = response, 
+      error: error => console.log(error),
+      complete: () => console.log("request completed")
+    })
+  }
+  
 }
+ 
